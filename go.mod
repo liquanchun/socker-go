d054db9e5f2f0e38e8b429541ead0cb2330f21a2
李全春 <350966707@qq.com>
2020-01-16 20:51:39 +0800

    update

--- go.mod
+++ go.mod
@@ -6,7 +6,11 @@ require (
 	github.com/firstrow/tcp_server v0.0.0-20190424084220-b7a05ff2879d
 	github.com/gin-gonic/gin v1.5.0
 	github.com/henrylee2cn/erpc/v6 v6.3.1
+	github.com/leesper/holmes v0.0.0-20190916004209-307d8a307864
+	github.com/leesper/tao v0.0.0-20180126151721-50d6193148cc
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/newtools/zsocket v0.0.0-20180504210555-167e205a4337
+	github.com/spiral/goridge v2.1.4+incompatible
 	github.com/tidwall/evio v1.0.7
 	go.uber.org/atomic v1.5.1 // indirect
 	go.uber.org/multierr v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
-import "github.com/tidwall/evio"
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	SERVER_IP       = "127.0.0.1"
+	SERVER_PORT     = 8080
+	SERVER_RECV_LEN = 10
+)
 
 func main() {
-	var events evio.Events
-	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
-		out = in
-		return
+	address := SERVER_IP + ":" + strconv.Itoa(SERVER_PORT)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	if err := evio.Serve(events, "tcp://localhost:8080"); err != nil {
-		panic(err.Error())
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		defer conn.Close()
+		for {
+			data := make([]byte, SERVER_RECV_LEN)
+			_, err = conn.Read(data)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			strData := string(data)
+			fmt.Println("Received:", strData)
+
+			upper := strings.ToUpper(strData)
+			_, err = conn.Write([]byte(upper))
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println("Send:", upper)
+		}
 	}
 }
