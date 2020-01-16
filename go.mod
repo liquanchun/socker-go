037fafe8d39544be5e3fe27aad413dd035461a1d
liquanchun <liquanchun@gmail.com>
2020-01-16 17:31:28 +0800

    update

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module socker-go
 go 1.13
 
 require (
+	github.com/firstrow/tcp_server v0.0.0-20190424084220-b7a05ff2879d
 	github.com/gin-gonic/gin v1.5.0
 	github.com/henrylee2cn/erpc/v6 v6.3.1
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/tidwall/evio v1.0.7
 	go.uber.org/atomic v1.5.1 // indirect
 	go.uber.org/multierr v1.4.0 // indirect
 	go.uber.org/zap v1.13.0
--- main.go
+++ main.go
@@ -1,59 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/henrylee2cn/erpc/v6"
-)
+import "github.com/tidwall/evio"
 
 func main() {
-	defer erpc.FlushLogger()
-	// graceful
-	go erpc.GraceSignal()
-
-	// server peer
-	srv := erpc.NewPeer(erpc.PeerConfig{
-		CountTime:   true,
-		ListenPort:  8000,
-		PrintDetail: true,
-	})
-	// srv.SetTLSConfig(erpc.GenerateTLSConfigForServer())
-
-	// router
-	srv.RouteCall(new(Math))
-	// broadcast per 5s
-	go func() {
-		for {
-			time.Sleep(time.Second * 60)
-			srv.RangeSession(func(sess erpc.Session) bool {
-				sess.Push(
-					"/push/status",
-					fmt.Sprintf("this is a broadcast, server time: %v", time.Now()),
-				)
-				return true
-			})
-		}
-	}()
-
-	// listen and serve
-	srv.ListenAndServe()
-}
-
-// Math handler
-type Math struct {
-	erpc.CallCtx
-}
-
-// Add handles addition request
-func (m *Math) Add(arg *[]int) (int, *erpc.Status) {
-	// test meta
-	erpc.Infof("author: %s", m.PeekMeta("author"))
-	// add
-	var r int
-	for _, a := range *arg {
-		r += a
+	var events evio.Events
+	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
+		out = in
+		return
 	}
-	// response
-	return r, nil
+	if err := evio.Serve(events, "tcp://localhost:8080"); err != nil {
+		panic(err.Error())
+	}
 }
